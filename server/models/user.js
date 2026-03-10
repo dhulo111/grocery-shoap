b8e23b6989a35b240f4e7dde7cd4efa1c35cd0f8
dhulo_11 <dhruvanpatel8866@gmail.com>
2026-03-10 13:07:03 +0530

    user model created

---
+++ server/models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: { type: String },
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
